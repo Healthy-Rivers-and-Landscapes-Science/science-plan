497dfb0e8dd03478fe1f5dd50cc83df536f9acfd
Lucy Andrews <lucy.andrews@water.ca.gov>
2024-11-17 20:19:09 -0800

    add more packages

--- workshop/delta_smelt/globals.R
+++ workshop/delta_smelt/globals.R
@@ -1,8 +1,12 @@
 # packages
 library(dplyr)
+library(EDIutils)
 library(ggplot2)
 library(here)
+library(readr)
 library(sf)
+library(snakecase)
+library(stringr)
 
 # spatial global variables
 global_crs <- 4269
