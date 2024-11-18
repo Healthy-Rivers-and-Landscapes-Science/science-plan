5a97f75f4cca0f8bf98670a392f27d4f74daab09
Lucy Andrews <lucy.andrews@water.ca.gov>
2024-11-17 16:37:25 -0800

    change object names

--- workshop/habs/data-raw/emp_dwq.R
+++ workshop/habs/data-raw/emp_dwq.R
@@ -23,13 +23,13 @@ for (i in seq_len(nrow(entities))) {
 }
 
 # clean data
-colnames(emp_dwq_1975_2023) <- snakecase::to_snake_case(
-  string = colnames(emp_dwq_1975_2023),
+colnames(emp_dwq_stations_1975_2023) <- snakecase::to_snake_case(
+  string = colnames(emp_dwq_stations_1975_2023),
   parsing_option = 1
 )
 
-colnames(emp_dwq_stations_1975_2023) <- snakecase::to_snake_case(
-  string = colnames(emp_dwq_stations_1975_2023),
+colnames(emp_dwq_1975_2023) <- snakecase::to_snake_case(
+  string = colnames(emp_dwq_1975_2023),
   parsing_option = 1
 )
 
@@ -40,22 +40,32 @@ emp_dwq_1975_2023 <- dplyr::select(
   microcystis
 )
 
-emp_dwq_1975_2023 <- dplyr::filter(emp_dwq_1975_2023, !is.na(microcystis))
+microcystis_all <- dplyr::filter(
+  emp_dwq_1975_2023,
+  !is.na(microcystis),
+  date >= as.Date("2015-01-01")
+)
 
 # add columns needed for analysis
-emp_dwq_1975_2023 <- dplyr::mutate(
-  emp_dwq_1975_2023,
+microcystis_all <- dplyr::mutate(
+  microcystis_all,
+  severity_cat = dplyr::case_when(
+    microcystis >= 4 ~ "high",
+    microcystis >= 2 ~ "low",
+    microcystis == 1 ~ "absent"
+  ) |>
+    factor(levels = c("absent", "low", "high")),
   year = lubridate::year(date),
   month = lubridate::month(date),
   season = dplyr::case_when(
-    month %in% c(11, 12, 1, 2) ~ "winter",
-    month %in% c(3, 4, 5, 6, 7, 8, 9, 10) ~ "summer",
+    month %in% c(12, 1, 2, 3, 4) ~ "winter",
+    month %in% c(5, 6, 7, 8, 9, 10, 11) ~ "summer",
     TRUE ~ NA_character_
   )
 )
 
 # summarize sites by season
-microcystis <- emp_dwq_1975_2023 |>
+microcystis_summary <- microcystis_all |>
   dplyr::group_by(station, season) |>
   dplyr::summarize(
     microcystis_90th = {
@@ -67,8 +77,8 @@ microcystis <- emp_dwq_1975_2023 |>
   )
 
 # create factors needed for visualization
-microcystis <- dplyr::mutate(
-  microcystis,
+microcystis_summary <- dplyr::mutate(
+  microcystis_summary,
   season = factor(season, levels = c("winter", "summer")),
   microcystis_90th_bin = dplyr::case_when(
     microcystis_90th >= 4 ~ "high",
@@ -84,17 +94,31 @@ microcystis <- dplyr::mutate(
     factor(levels = c("absent", "low", "high"))
 )
 
-# add coordinates and convert microcystis summary to an sf object
-microcystis <- dplyr::left_join(
-  x = microcystis,
+# add coordinates and convert microcystis dataframes to sf objects
+microcystis_all <- dplyr::left_join(
+  x = microcystis_all,
   y = dplyr::select(emp_dwq_stations_1975_2023, station, latitude, longitude),
   by = "station"
 )
 
-microcystis <- dplyr::filter(microcystis, !is.na(latitude))
+microcystis_all <- dplyr::filter(microcystis_all, !is.na(latitude))
 
-microcystis <- sf::st_as_sf(
-  x = microcystis,
+microcystis_all <- sf::st_as_sf(
+  x = microcystis_all,
+  coords = c("longitude", "latitude"),
+  crs = global_crs
+)
+
+microcystis_summary <- dplyr::left_join(
+  x = microcystis_summary,
+  y = dplyr::select(emp_dwq_stations_1975_2023, station, latitude, longitude),
+  by = "station"
+)
+
+microcystis_summary <- dplyr::filter(microcystis_summary, !is.na(latitude))
+
+microcystis_summary <- sf::st_as_sf(
+  x = microcystis_summary,
   coords = c("longitude", "latitude"),
   crs = global_crs
 )
@@ -113,10 +137,16 @@ emp_stations <- sf::st_as_sf(
   dim = "XY"
 )
 
+# save all microcystis data
+save(
+  microcystis_all,
+  file = here::here("workshop", "habs", "data", "microcystis_all.rda")
+)
+
 # save microcystis summary data
 save(
-  microcystis,
-  file = here::here("workshop", "habs", "data", "microcystis.rda")
+  microcystis_summary,
+  file = here::here("workshop", "habs", "data", "microcystis_summary.rda")
 )
 
 # save stations data
