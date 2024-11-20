bafb1341afa908b174f06e8c8723cf8b2eb922de
Lucy Andrews <lucy.andrews@water.ca.gov>
2024-11-19 16:18:52 -0800

    change sine interpolation function to have the option of modifying it by a power

--- workshop/feather_river/functions/interpolate_sine.R
+++ workshop/feather_river/functions/interpolate_sine.R
@@ -1,5 +1,6 @@
-# create a function that interpolates between two coordinate pairs with a sine curve
-interpolate_sine <- function(x_start, x_end, y_start, y_end, n) {
+# create a function that interpolates between two coordinate pairs using a
+# power-transformed sine function
+interpolate_sine <- function(x_start, x_end, y_start, y_end, n, power = 2) {
   # generate n equally spaced x values between x_start and x_end
   x_values <- seq(x_start, x_end, length.out = n)
 
@@ -7,8 +8,11 @@ interpolate_sine <- function(x_start, x_end, y_start, y_end, n) {
   amplitude <- (y_end - y_start) / 2
   y_center <- (y_start + y_end) / 2
 
-  # scale x values to cover a half sine wave, ensuring it goes from -1 to 1
-  y_values <- y_center + amplitude * sin((x_values - x_start) * (pi / (x_end - x_start)) - (pi / 2))
+  # scale x values to cover a half sine wave, ensuring it goes from -pi/2 to pi/2
+  normalized_x <- (x_values - x_start) / (x_end - x_start) * pi - (pi / 2)
+
+  # modify the sine curve to make it steeper in the middle while preserving symmetry
+  y_values <- y_center + amplitude * sign(sin(normalized_x)) * abs(sin(normalized_x))^power
 
   # return a dataframe of x and y coordinates
   return(data.frame(x = x_values, y = y_values))
